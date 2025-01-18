339aa5e8b830686ecc9701ff194e4f0b538fa76f
Krutik Naina <nenakrutik2004@gmail.com>
2025-01-18 15:25:38 +0530

    edit commit

--- src/Componets/Mealnfo.js
+++ src/Componets/Mealnfo.js
@@ -6,22 +6,33 @@ const Mealnfo = () => {
     const [info, setInfo] = useState()
     console.log(mealid);
 
+    // const getInfo = async () => {
+    //     const get = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`)
+    //     const jsonData = await get.json();
+    //     console.log(jsonData.meals[0]);
+    //     setInfo(jsonData.meals[0]);
+    // }
+    // if (info != "") {
+    //     getInfo()
+    // }
+
     const getInfo = async () => {
         const get = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`)
         const jsonData = await get.json();
         console.log(jsonData.meals[0]);
         setInfo(jsonData.meals[0]);
     }
-    if (info != "") {
+    if (!info) {
         getInfo()
     }
+
 
     return (
         <div>
             {
                 !info ? "Data Not Found" :
                     <div className='mealInfo'>
-                        <img src={info.strMealThumb}/>
+                        <img src={info.strMealThumb} />
                         <div className='info'>
                             <h1>Recipe Details</h1>
                             <button>{info.strMeal}</button>
